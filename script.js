789743522db7715ce03172e74f89dc851e753595
dediane <dianedecourt@gmail.com>
2020-11-16 17:24:38 +0100

    first commit 1 to 4

---
+++ script.js
@@ -0,0 +1,51 @@
+//fonctionnalité 1 & 1-bis
+function clique_footer(){
+    var x = 0;
+
+    function actionFooter(){
+        x++;
+        console.log("clique " + x + " fois") 
+    }
+    document.getElementsByTagName('footer')[0].addEventListener("click", actionFooter);
+}
+
+//fonctionnalité 2
+function collapse_hamburger()
+{
+    var headerNav = document.getElementById("navbarHeader");
+    var buttonNav = document.querySelector("button.navbar-toggler");
+    buttonNav.addEventListener("click", () => {
+        headerNav.classList.toggle("collapse");
+    });
+}
+collapse_hamburger();
+
+//fonctionnalité 3
+function edit_red(){
+    var CardElement = document.getElementsByClassName('card')[0]
+    var ButtonElement = document.querySelector(".btn-outline-secondary");
+    ButtonElement.addEventListener("click", () =>{
+        CardElement.style="color:red"
+    });
+}
+edit_red();
+
+//fonctionnalité 4
+function edit_green(){
+    var CardElement= document.getElementsByClassName('card')[1]
+    var ButtonElement = CardElement.querySelector(".btn-outline-secondary");
+    var flag = false;
+    ButtonElement.addEventListener("click", () =>{
+        if (flag === false)
+        {
+            CardElement.style="color:green";
+            flag = true;
+        }
+        else
+        {
+            CardElement.style="color:black";
+            flag = false;
+        }
+    });
+}
+edit_green();
